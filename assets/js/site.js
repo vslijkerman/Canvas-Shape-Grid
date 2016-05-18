ed2b85f19e307de56a89fe306633eb0cc9bc90b8
Vincent Slijkerman <v.slijkerman@gmail.com>
2016-05-18 09:16:27 +0200

    Refactored the code

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,23 +5,25 @@ $(document).ready(function() {
   // Construct a default dot.
   var Dot = function(x, y, color, radius) {
     if(color == null) {
-      this.color = 'rgba(255,255,255,0.4)';
+      this.color = 'rgba(255,255,255,0.2)';
     } else {
       this.color = color;
     }
     if(radius == null) {
-      this.radius = 4;
+      this.radius = 24;
     } else {
       this.radius = radius;
     }
-    this.x = (x + this.radius); // The arc's center is at the given x coord. This way the top left side is at the given coords instead of the arc-center.
-    this.y = (y + this.radius); // The arc's center is at the given y coord. This way the top left side is at the given coords instead of the arc-center.
+    this.x = (x + this.radius); // The dot's center is by default at the given x coord. This way the top left side is at the given coords instead of the arc-center.
+    this.y = (y + this.radius); // The dot's center is by default at the given y coord. This way the top left side is at the given coords instead of the arc-center.
     this.draw = function() {
+      ctx.save();
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
       ctx.closePath();
       ctx.fillStyle = this.color;
       ctx.fill();
+      ctx.restore();
     }
   };
 
@@ -30,21 +32,22 @@ $(document).ready(function() {
     this.x = x;
     this.y = y;
     if(width == null) {
-      this.width = 8;
+      this.width = 48;
     } else {
       this.width = width;
     }
     if(height == null) {
-      this.height = 8;
+      this.height = 48;
     } else {
       this.height = height;
     }
     if(color == null) {
-      this.color = 'rgba(255,255,255,0.4)';
+      this.color = 'rgba(255,255,255,0.2)';
     } else {
       this.color = color;
     }
     this.draw = function() {
+      ctx.save();
       ctx.beginPath();
       ctx.rect(this.x, this.y, this.width, this.height);
       ctx.closePath();
@@ -57,34 +60,27 @@ $(document).ready(function() {
         ctx.lineWidth = stroke;
         ctx.stroke();
       }
+      ctx.restore();
     }
   };
 
   // An array to keep all the dot objects in one place.
-  var dots = [];
+  var shapes = [];
   // Variable used to temporarily store dot object.
-  var dot;
-
-  // An array to keep all the rectangle objects in one place.
-  var rectangles = [];
-  // Variable used to temporarily store rectangle object.
-  var rectangle;
+  var shape;
 
   // Place your objects below. Example:
   //
-  // dot = new Dot(0, 0);
-  // dots.push(dot);
+  // shape = new Dot(0, 0);
+  // shapes.push(shape);
   //
-  // rectangle = new Rect(0, 0);
-  // rectangles.push(rectangle);
+  // or...
+  //
+  // shape = new Rect(0, 0);
+  // shapes.push(shape);
 
-  // Loop through the dots array and draw all the dots.
-  for(i = 0; i < dots.length; i++) {
-    dots[i].draw();
-  }
-
-  // Loop through the rectangles array and draw all the rectangles.
-  for(i = 0; i < rectangles.length; i++) {
-    rectangles[i].draw();
+  // Loop through the shapes array and draw all the shapes.
+  for(i = 0; i < shapes.length; i++) {
+    shapes[i].draw();
   }
 });
